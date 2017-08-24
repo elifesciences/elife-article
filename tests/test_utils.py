84fe9aa206e0c386de5236b8606fcb0005133cc5
Graham Nott <gnott@starglobal.ca>
2017-08-23 17:00:31 -0700

    Remove some output escaping functions already moved to elifetools.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -27,56 +27,9 @@ class TestUtils(unittest.TestCase):
             self.assertEqual(utils.entity_to_unicode(
                 string_input), string_output)
 
-    def test_xml_escape_ampersand(self):
-        self.passes = []
-        self.passes.append(('',
-                           u''))
-        self.passes.append(('a',
-                           u'a'))
-        self.passes.append(('another & another & another',
-                           u'another &amp; another &amp; another'))
-        self.passes.append(('a&a',
-                           u'a&amp;a'))
-        self.passes.append(('a&amp;&a',
-                           u'a&amp;&amp;a'))
-        self.passes.append(('a&#x0117;a',
-                           u'a&#x0117;a'))
-        self.passes.append(('fake link http://example.org/?a=b&amp',
-                           u'fake link http://example.org/?a=b&amp;amp'))
-        self.passes.append(('CUT&RUN is performed',
-                           u'CUT&amp;RUN is performed'))
-
-        for string_input, string_output in self.passes:
-            self.assertEqual(utils.xml_escape_ampersand(
-                string_input), string_output)
-
     def test_replace_tags(self):
         self.assertEqual(utils.replace_tags("<i>"), "<italic>")
 
-    def test_escape_unmatched_angle_brackets(self):
-        """
-        Test some additional examples of unmatched angle brackets specifically
-        """
-        self.passes = []
-        self.passes.append(('''<p><i><italic><b><bold><sup><sub><sc><u><underline><b>
-                            <bold>superstyling</bold>
-                            </b></underline></u></sc></sub></sup></bold></b></italic></i></p>''',
-                            '''<p><i><italic><b><bold><sup><sub><sc><u><underline><b>
-                            <bold>superstyling</bold>
-                            </b></underline></u></sc></sub></sup></bold></b></italic></i></p>'''))
-
-        self.passes.append(('<',
-                           '&lt;'))
-
-        self.passes.append(('< T << G << C >> A <<i>m</i>',
-                           '&lt; T &lt;&lt; G &lt;&lt; C &gt;&gt; A &lt;<i>m</i>'))
-
-        self.passes.append(('<p>**p<0.01; ***p<0.001. SI, aged mice >5 months old.</p>',
-                           '<p>**p&lt;0.01; ***p&lt;0.001. SI, aged mice &gt;5 months old.</p>'))
-
-        for string_input, string_output in self.passes:
-            self.assertEqual(utils.escape_unmatched_angle_brackets(
-                string_input), string_output)
 
 if __name__ == '__main__':
     unittest.main()
