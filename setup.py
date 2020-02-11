4926e5a62d2c6176b714dd29943cea66ff57fb8f
Luke <lsh-0@users.noreply.github.com>
2020-02-11 11:57:32 +1030

    tox, changes py35 environment to just py3

--- setup.py
+++ setup.py
@@ -23,6 +23,5 @@ setup(name='elifearticle',
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         ]
     )
