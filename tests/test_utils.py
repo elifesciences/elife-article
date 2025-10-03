d0640ff3607fb8e2504a3fe50dd78f5b5b498c95
Graham Nott <gnott@starglobal.ca>
2025-10-03 16:27:24 -0700

    Remove old entity_to_unicode() function, moved to elifetools

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,45 +12,6 @@ class TestUtils(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_repl(self):
-        string = "&#x2022;"
-        matches = re.match(r"&#x(....);", string)
-        self.assertEqual(utils.repl(matches), "\u2022")
-
-    def test_entity_to_unicode(self):
-        passes = []
-        passes.append(
-            (
-                "N-terminal &#x03B1;-helix into the heterodimer interface",
-                "N-terminal \u03b1-helix into the heterodimer interface",
-            )
-        )
-
-        passes.append(
-            (
-                "N-terminal &alpha;-helix into the heterodimer interface",
-                "N-terminal \u03b1-helix into the heterodimer interface",
-            )
-        )
-
-        passes.append(
-            (
-                (
-                    "&#x00A0; &#x00C5; &#x00D7; &#x00EF; &#x0394; &#x03B1; &#x03B2; &#x03B3;"
-                    " &#x03BA; &#x03BB; &#x2212; &#x223C; &alpha; &amp; &beta; &epsilon; &iuml;"
-                    " &ldquo; &ordm; &rdquo;"
-                ),
-                (
-                    "\xa0 \xc5 \xd7 \xef \u0394 \u03b1 \u03b2 \u03b3"
-                    " \u03ba \u03bb \u2212 \u223c \u03b1 &amp; \u03b2 \u03b5 \xcf "
-                    '" \xba "'
-                ),
-            )
-        )
-
-        for string_input, string_output in passes:
-            self.assertEqual(utils.entity_to_unicode(string_input), string_output)
-
     def test_remove_tag(self):
         self.assertEqual(utils.remove_tag("i", "<i>test</i>"), "test")
         self.assertEqual(utils.remove_tag("i", None), None)
